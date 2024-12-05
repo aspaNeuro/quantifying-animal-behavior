2fd456ec6af780aa162f9ad698132e617e83e022
AspaNeuro <a.almeida@neurogears.org>
2024-12-05 11:27:06 +0000

    Add a packawge example

---
+++ PackageExample/PackageExample/SampleSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai;
+
+namespace PackageExample
+{
+    [Description("SampleSource")]
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class SampleSource
+    {
+        int value;
+        event Action<int> ValueChanged;
+
+        /// <summary>
+        /// Gets or sets the value of the property.
+        /// </summary>
+        [Range(int.MinValue, int.MaxValue)]
+        [Description("The value of the property.")]
+        [Editor(DesignTypes.NumericUpDownEditor, DesignTypes.UITypeEditor)]
+        public int Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnValueChanged(value);
+            }
+        }
+        void OnValueChanged(int value)
+        {
+            ValueChanged?.Invoke(value);
+        }
+        public IObservable<int> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(value))
+            .Concat(Observable.FromEvent<int>(
+            handler => ValueChanged += handler,
+            handler => ValueChanged -= handler));
+        }
+    }
+}
